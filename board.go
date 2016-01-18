06e4f345a5abd4ef88c91591bff936b7038ba5fe
William Hipschman <willhi@microsoft.com>
2016-01-18 00:07:17 -0500

    Add bomb setup

--- board.go
+++ board.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 import "errors"
+import "math/rand"
 
 var(
     INVALID_BOMBS = "The number of bombs must be greater than 0 and less than width * height."
-    DIMENSION_TOO_SMALL = "The dimension cannot be less than 0."
+    DIMENSION_TOO_SMALL = "The dimension cannot be less than 1."
     DIMENSION_TOO_LARGE = "The dimension cannot be greater than 80 for width and 40 for height."
-    HEIGHT_WIDTH_NOT_SET = "You can declare the number of bombs until height and width have been set."
+    HEIGHT_WIDTH_NOT_SET = "Height and Width must be set for this operation to complete."
 )
 
 type setAction func (val int)(error)
@@ -16,24 +17,29 @@ type Board struct{
     width int
     height int
     numOfBombs int
+    field [][]rune
 }
 
 func (board *Board) Init(){
     setBoardAttribute(board.SetWidth, "Enter the width of the board.")
     setBoardAttribute(board.SetHeight, "Enter the height of the board.")
     setBoardAttribute(board.SetBombs, "Enter the number of bombs.")
+    
+    if err := board.initBombs(); err != nil{
+        board.Init()
+    }
 }
 
 func (board *Board) SetBombs(numOfBombs int)(error){
-    if(numOfBombs <= 0){
+    if (numOfBombs <= 0){
         return errors.New(INVALID_BOMBS)
     }
     
-    if(board.width == 0  || board.height == 0){
-        return errors.New(HEIGHT_WIDTH_NOT_SET)
+    if err := board.validateHeightAndWidthAreSet(); err != nil {
+        return err
     }
     
-    if(numOfBombs >= board.width * board.height){
+    if (numOfBombs >= board.width * board.height){
         return errors.New(INVALID_BOMBS)
     }
     
@@ -42,11 +48,11 @@ func (board *Board) SetBombs(numOfBombs int)(error){
 }
 
 func (board *Board) SetHeight(height int)(error){
-    if(height <= 0){
+    if (height <= 1){
         return errors.New(DIMENSION_TOO_SMALL)
     }
     
-    if(height > 40){
+    if (height > 40){
         return errors.New(DIMENSION_TOO_LARGE)
     }
     
@@ -55,15 +61,55 @@ func (board *Board) SetHeight(height int)(error){
 }
 
 func (board *Board) SetWidth(width int)(error){
-    if(width <= 0){
+    if (width <= 1){
         return errors.New(DIMENSION_TOO_SMALL)
     }
     
-    if(width > 80){
+    if (width > 80){
         return errors.New(DIMENSION_TOO_LARGE)
     }
     
     board.width = width
+    return nil
+}
+
+func (board *Board) initBombs() error{
+    
+    // A cell containing a * is a bomb
+    // A cell containing 0 is undiscovered and may be explorer
+    // A cell containing 1 is discovered and may not be explored
+    board.field = make([][]rune, board.width, board.height)
+    
+    if err := board.generateBombPositions(); err != nil{
+        return err
+    }
+    
+    return nil
+}
+
+func (board *Board) generateBombPositions() error{
+    if err := board.validateHeightAndWidthAreSet(); err != nil{
+        return err
+    }
+    
+    for numberToGenerate := board.numOfBombs; numberToGenerate > 0;{
+        row := rand.Int()
+        col := rand.Int()
+        
+        if (board.field[row][col] == 0){
+            board.field[row][col] = '*'
+            numberToGenerate--
+        }
+    }
+    
+    return nil
+}
+
+func (board *Board) validateHeightAndWidthAreSet() error{
+    if(board.width == 0  || board.height == 0){
+        return errors.New(HEIGHT_WIDTH_NOT_SET)
+    }
+    
     return nil
 }
 
