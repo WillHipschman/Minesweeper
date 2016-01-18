e00667ade91199c304d0cf3fdf2f92084f8f2e93
William Hipschman <willhi@microsoft.com>
2016-01-18 00:11:05 -0500

    Add Init Validation Test

--- board_test.go
+++ board_test.go
@@ -4,6 +4,23 @@ import "testing"
 import "fmt"
 import "github.com/willhipschman/minesweeper/resources"
 
+func TestValidateHeightAndWidthAreSet(t *testing.T){
+    board := new(Board)
+    
+    failIfNull(t, board.validateHeightAndWidthAreSet(), "ValidateHeightAndWidthAreSet should fail before init.")
+    
+    board.SetHeight(20)
+    board.SetWidth(20)
+    
+    failIfNotNull(t, board.validateHeightAndWidthAreSet(), "ValidateHeightAndWidthAreSet should succeed after init.")
+}
+
+func TestGenerateBombPositions(t *testing.T){
+    board := new(Board)
+    
+    board.SetHeight(20)
+    board.SetWidth(20)
+}
 
 func TestSetBombs(t *testing.T){
     board := new(Board)
@@ -33,11 +50,12 @@ func TestSetHeight(t *testing.T){
     failIfNull(t, board.SetHeight(resources.MinInt), "MinInt is an illegal height.")
     failIfNull(t, board.SetHeight(-1), "-1 is an illegal height.")
     failIfNull(t, board.SetHeight(0), "0 is an illegal height.")
+    failIfNull(t, board.SetHeight(1), "1 is an illegal height.")
     failIfNull(t, board.SetHeight(41), "41 is an illegal height.")
     failIfNull(t, board.SetHeight(resources.MaxInt), "MaxInt is an illegal height.")
     
-    failIfNotNull(t, board.SetHeight(1), "1 should be a legal value.")
-    assertIsEqual(t, board.height, 1, "Height should have been %d but is %d")
+    failIfNotNull(t, board.SetHeight(2), "2 should be a legal value.")
+    assertIsEqual(t, board.height, 2, "Height should have been %d but is %d")
     
     failIfNotNull(t, board.SetHeight(27), "27 should be a legal value.")
     assertIsEqual(t, board.height, 27, "Height should have been %d but is %d")
@@ -54,11 +72,12 @@ func TestSetWidth(t *testing.T){
     failIfNull(t, board.SetWidth(resources.MinInt), "MinInt is an illegal width.")
     failIfNull(t, board.SetWidth(-1), "-1 is an illegal width.")
     failIfNull(t, board.SetWidth(0), "0 is an illegal width.")
+    failIfNull(t, board.SetWidth(1), "1 is an illegal width.")
     failIfNull(t, board.SetWidth(81), "81 is an illegal width.")
     failIfNull(t, board.SetWidth(resources.MaxInt), "MaxInt is an illegal width.")
     
-    failIfNotNull(t, board.SetWidth(1), "1 should be a legal value.")
-    assertIsEqual(t, board.width, 1, "Width should have been %d but is %d")
+    failIfNotNull(t, board.SetWidth(2), "2 should be a legal value.")
+    assertIsEqual(t, board.width, 2, "Width should have been %d but is %d")
     
     failIfNotNull(t, board.SetWidth(37), "37 should be a legal value.")
     assertIsEqual(t, board.width, 37, "Width should have been %d but is %d")
